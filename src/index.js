392b5c807f691a846811c6cb44cf65523fdd683d
Victor <victorstafy11@gmail.com>
2022-09-25 11:39:59 -0300

    implementacao da api concluida

--- src/index.js
+++ src/index.js
@@ -105,11 +105,11 @@ server.post('/choice',async (req,res)=>{
       return;
     }
 
-    await db.collection("choice").insertOne({
+    choice_obj=await db.collection("choice").insertOne({
       title: choice.title, pollId: choice.pollId, 
     })
 
-    return res.status(201).send(choice.pollId);
+    return res.status(201).send(choice_obj);
 
   } catch (error) {
     return res.status(500).send(error);
@@ -119,7 +119,7 @@ server.post('/choice',async (req,res)=>{
 server.get('/poll/:id/choice',async (req,res)=>{
   const _id= req.params;
   try{
-      const choice_list= await db.collection("choice").find().toArray();
+      const choice_list= await db.collection("choice").find({ pollId: { $eq: _id } }).toArray();
       if (!choice_list){
         res.sendStatus(404);
         return;
@@ -128,6 +128,69 @@ server.get('/poll/:id/choice',async (req,res)=>{
         res.send(choice_list);
         return;
       }
+  }
+  catch{
+    return res.status(500);
+  } 
+})
+
+server.post('/choice:id/vote',async (req,res)=>{
+  const choiceId= req.params;
+  const today = dayjs(new Date());
+
+  try {
+//
+    const choice=await db.collection("choice").findOne({_id:choiceId});
+    const poll=await db.collection("poll").findOne({_id:choice.pollId});
+
+    if (!choice){
+      res.sendStatus(404);
+      return;
+    }
+    if (poll.expireAt.isBefore(today)){
+      res.sendStatus(403);
+      return;
+    }
+
+    await db.collection("vote").insertOne({
+      choiceId: choiceId, choiceTitle:choice.title, pollId: choice.pollId, vote: 1, 
+      date:dayjs().format('YYYY/MM/DD HH:mm')
+    })
+//
+    return res.status(201);
+
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+server.get('/poll/:id/result',async (req,res)=>{
+  const pollId= req.params;
+
+  try{
+    const poll=await db.collection("poll").findOne({_id:pollId});
+    
+    if (!poll){
+      res.sendStatus(404);
+      return;
+    }
+
+    const choice_list= await db.collection("choice").find({ pollId: { $eq: pollId } }).toArray();
+    const id_choice_list=choice_list.map(choice=>choice._id);
+    const votes_list=await id_choice_list.map(id_choice=>db.collection("vote").find({choiceId: { $eq: id_choice }}).toArray())
+    const votes_count=await votes_list.map(vote_list_per_choice=>vote_list_per_choice.length);
+    const votes_choice_title=await votes_list.map(vote_obj_list=>vote_obj_list[0].choiceTitle);
+    const max_votes=Math.max(...votes_count)
+    const max_votes_index=votes_count.indexOf(max_votes);
+    const chosen_choice= votes_list[max_votes_index].choiceTitle;
+
+    const new_poll_obj={...poll};
+    new_poll_obj.result={
+      title: chosen_choice,
+      votes: max_votes
+    }
+    return res.send(new_poll_obj);
+
   }
   catch{
     return res.status(500);
